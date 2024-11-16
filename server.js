ca5c4fe0ecdba4b7b3bf6ee923cccd7ae4b6c9e4
Fariha <farihaorna@gmail.com>
2024-11-16 15:50:22 -0500

    enhanced addBook function so that it would store new book details in local storage

--- server.js
+++ server.js
@@ -3,28 +3,44 @@ let addBookBtn = document.querySelector('.add-book-btn')
 let bookName = document.getElementById("book-name")
 let authorName = document.getElementById("author")
 let genreName = document.getElementById("genre")
+let  addedBooksArray = []
+
+//variable
+let userBookGenre = "Fantasy"
 
 //This function will get the genre the user chose from the option
 const getGenreHandler = (e) => {
-  let userBookGenre = e.target.value
-  return userBookGenre
+  userBookGenre = e.target.value
 }
 
 //This function would store all the information from the form and store it in local storage
 const addBookBtnHandler = () => {
   let bookInfo = {}
   if (bookName.value){
+    bookInfo["book"] = bookName.value
 
   }else{
     bookName.style.border = "0.2em solid red"
   }
 
   if(authorName.value){
-
+    bookInfo["author"] = authorName.value
   }else{
     authorName.style.border = "0.2em solid red"
   }
 
+  bookInfo["genre"] = userBookGenre;
+
+  //add new book to local storage
+  if(bookName.value && authorName.value){
+    addedBooksArray.push(bookInfo)
+    localStorage.setItem("addBooks",JSON.stringify(addedBooksArray))
+  }
+
+  //clearing out the input fields back to default
+  bookName.value = ""
+  authorName.value = ""
+  
 }
 
 //adding event listners
